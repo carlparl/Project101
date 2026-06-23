0f58b2cbb170ed20a2d0b05e1d15c1f31f660547
carlparl <grahamwalz1@gmail.com>
2026-06-23 17:26:42 +0300

    Add missing migration files

---
+++ core/management/commands/createsuperuser_auto.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates a superuser automatically if it does not exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@shafnettours.com', 'q1w2e3r4')
+            self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
